cacc7fce7e81412168a9ae2e0ebae84195527e67
AlexandraGrecu <alexandragrecu344@gmail.com>
2026-07-28 13:02:59 +0300

    Refactor: remove undefined check and convert px to rem

--- src/components/molecules/ProductPrice/ProductPrice.tsx
+++ src/components/molecules/ProductPrice/ProductPrice.tsx
@@ -10,7 +10,7 @@ const ProductPrice: React.FC<ProductPriceProps> = ({ currentPrice, oldPrice }) =
   return (
     <div className="product-price">
       <span className="product-price__current">${currentPrice.toFixed(2)}</span>
-      {oldPrice !== undefined && (
+      {oldPrice && (
         <span className="product-price__old">${oldPrice.toFixed(2)}</span>
       )}
     </div>
