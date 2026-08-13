72be4561baf51c1155cee52c20049343c5c85a3c
AlexandraGrecu <alexandragrecu344@gmail.com>
2026-08-13 19:06:28 +0300

    feat: MINI-TASK-94 - functional sidebar category, price slider, color and condition filters applied on Filter button click

--- src/components/organisms/Sidebar/SidebarCategory.tsx
+++ src/components/organisms/Sidebar/SidebarCategory.tsx
@@ -4,6 +4,8 @@ import ClothingIcon from '../../../assets/ClothingBeautyIcon.svg';
 import PetFoodIcon from '../../../assets/PetFoodToyIcon.svg';
 import BakingIcon from '../../../assets/bakingMaterialIcon.svg';
 import FruitIcon from '../../../assets/VegetableIcon.svg';
+import { useSearch } from '../../../context/SearchContext';
+import { allProducts } from '../../../utils/mockData';
 import './SidebarCategory.css';
 
 interface CategoryItem {
@@ -22,20 +24,47 @@ const categories: CategoryItem[] = [
 ];
 
 export const SidebarCategory: React.FC = () => {
+  const { selectedCategory, setSelectedCategory } = useSearch();
+
+  const handleCategoryClick = (categoryName: string) => {
+    if (selectedCategory === categoryName) {
+      setSelectedCategory('All Categories');
+    } else {
+      setSelectedCategory(categoryName);
+    }
+  };
+
   return (
     <div className="sidebar-widget sidebar-category">
       <h3 className="sidebar-widget__title">Category</h3>
       <div className="sidebar-category__line"></div>
       <ul className="sidebar-category__list">
-        {categories.map((cat) => (
-          <li key={cat.id} className="sidebar-category__item">
-            <div className="sidebar-category__item-left">
-              <img src={cat.icon} alt={cat.name} className="sidebar-category__icon" />
-              <span className="sidebar-category__name">{cat.name}</span>
-            </div>
-            <span className="sidebar-category__count">{cat.count}</span>
-          </li>
-        ))}
+        {categories.map((cat) => {
+          const isActive = selectedCategory === cat.name;
+          const matchingCount = allProducts.filter((p) => p.category === cat.name).length;
+          const displayCount = matchingCount > 0 ? matchingCount : cat.count;
+
+          return (
+            <li
+              key={cat.id}
+              className={`sidebar-category__item ${isActive ? 'sidebar-category__item--active' : ''}`}
+              onClick={() => handleCategoryClick(cat.name)}
+              role="button"
+              tabIndex={0}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') {
+                  handleCategoryClick(cat.name);
+                }
+              }}
+            >
+              <div className="sidebar-category__item-left">
+                <img src={cat.icon} alt={cat.name} className="sidebar-category__icon" />
+                <span className="sidebar-category__name">{cat.name}</span>
+              </div>
+              <span className="sidebar-category__count">{displayCount}</span>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
